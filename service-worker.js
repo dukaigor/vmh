5d01d0089f131413b6bb21796efb19ef2a5179cd
dukaigor <dukaigor19989@gmail.com>
2024-09-06 03:04:25 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,23 @@
 const CACHE_NAME = 'vmh-tracker-v1';
 const urlsToCache = [
-  '/vmh/',
-  '/vmh/index.html',
-  '/vmh/manifest.json',
-  'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js',
-  'https://www.gstatic.com/firebasejs/9.22.0/firebase-storage-compat.js',
-  'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/logovmh-vUQp53LQUYf0HYaB3uFVoeNZtgxavL.png'
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+      .then(response => response || fetch(event.request))
   );
 });
